741c045d148a7d9b1beb5b3a2666b8cb2d1fce1f
Nicolas Tagliabue <n1ktagl1abue@gmail.com>
2017-06-18 17:23:22 +0200

    small fixes

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import random
 
 clu = open ("./wordnet3_adj.clu", "w")
-clu.write('*Vertices 82670')
+clu.write('*Vertices 82670\n')
 for i in range(0,82670):
-    clu.write("       " + str(random.randint(0,9)) + '\n')
+    clu.write("       " + str(random.randint(1,9)) + '\n')
 clu.close()
